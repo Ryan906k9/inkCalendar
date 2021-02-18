f5a0586347667d7b1468485f7319c45ba0eb6096
Ray <raistlin.chen@163.com>
2021-02-18 22:41:55 +0800

    ok

--- src/config.h
+++ src/config.h
@@ -1,4 +1,3 @@
 //TODO: 使用您自己的和风API KEY
 #include <WString.h>
-const String QWEATHER_API_KEY = "c90aee26439f4bf49403f69b40a9b690";
-
+const String QWEATHER_API_KEY = "*****************************";
